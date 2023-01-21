7d1bd58afa5205d77ab20fe9787a7c2d286a923e
RichPauloo <rpauloo@ucdavis.edu>
2023-01-21 01:15:59 -0800

    git config try

--- R/run.sh
+++ R/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Calling R script to download data and write plot"
-Rscript -e "source('./R/01_download_plot.R');"
+# echo "Calling R script to download data and write plot"
+# Rscript -e "source('./R/01_download_plot.R');"
 
-# echo "Commiting plot to Github"
-# if [[ "$(git status --porcelain)" != "" ]]; then
-#     git config --global user.name 'RichPauloo'
-#     git config --global user.email 'richpauloo@gmail.com'
-#     git add *.png
-#     git commit -m "Auto update daily plot"
-#     git push
-# fi
+echo "Commiting plot to Github"
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name 'RichPauloo'
+    git config --global user.email 'richpauloo@gmail.com'
+    git add --all
+    git commit -m "Auto update daily plot"
+    git push
+fi
