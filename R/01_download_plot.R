0049c735b126d551cfecb6479002015ac04b175a
RichPauloo <rpauloo@ucdavis.edu>
2023-01-21 02:10:06 -0800

    Update 01_download_plot.R

--- R/01_download_plot.R
+++ R/01_download_plot.R
@@ -45,10 +45,9 @@ p <- ggplot(df, aes(t, do)) +
     caption  = caption,
     x = "", y = ""
   ) +
-  theme_minimal() +
+  # theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
 
-cat("Default wd is:", getwd(), "\n")
 path_out <- paste0("png/", Sys.Date(), ".png")
 ggsave(path_out, p, height = 5, width = 7)
 cat("Saved plot to", path_out, "\n")
