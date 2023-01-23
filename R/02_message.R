c044c8df210aab948d1327bcce41140b28bcfceb
RichPauloo <rpauloo@ucdavis.edu>
2023-01-23 09:47:28 -0800

    working unicode red yellow green

--- R/02_message.R
+++ R/02_message.R
@@ -6,11 +6,11 @@ tw_sid            <- Sys.getenv("TWILIO_SID")
 tw_tok            <- Sys.getenv("TWILIO_TOKEN")
 tw_phone_number   <- Sys.getenv("TWILIO_PHONE_NUMBER")
 tw_target_number  <- Sys.getenv("TARGET_PHONE_NUMBER")
-# tw_target_number2 <- Sys.getenv("TARGET_PHONE_NUMBER2")
-# tw_target_number3 <- Sys.getenv("TARGET_PHONE_NUMBER3")
-# tw_target_number4 <- Sys.getenv("TARGET_PHONE_NUMBER4")
+tw_target_number2 <- Sys.getenv("TARGET_PHONE_NUMBER2")
+tw_target_number3 <- Sys.getenv("TARGET_PHONE_NUMBER3")
+tw_target_number4 <- Sys.getenv("TARGET_PHONE_NUMBER4")
 
-nums <- c(tw_target_number)#), tw_target_number2, tw_target_number3, tw_target_number4)
+nums <- c(tw_target_number, tw_target_number2, tw_target_number3, tw_target_number4)
 
 # PNG plot img url from github (does this work with private repo?)
 url_img <- paste0(
@@ -32,12 +32,12 @@ msg_high <- paste(
 )
 
 msg_medium <- paste(
-  "U+1F7E1 U+1F41F MEDIUM minimum dissolved oxygen:",
+  "\U0001F7E1 \U0001F41F MEDIUM minimum dissolved oxygen:",
   min_do, "mg/L."
 )
 
 msg_low <- paste(
-  "U+1F534 U+1F41F LOW minimum dissolved oxygen:",
+  "\U0001F534 \U0001F41F LOW minimum dissolved oxygen:",
   min_do, "mg/L."
 )
 
