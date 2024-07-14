c18157d14491d856d1c3489e925be55f93ac7e0a
RichPauloo <rpauloo@ucdavis.edu>
2024-07-14 12:34:38 -0400

    refactor url and approach because the service was down for 2 days

--- R/01_download_plot.R
+++ R/01_download_plot.R
@@ -5,26 +5,61 @@ library(dplyr)
 
 # tomorrow's date for the API query
 today <- Sys.Date() + 1
-cat("Running query for", as.character(today), "and 72 hrs prior.\n")
+cat("Running query for", as.character(today), "and 7 days prior.\n")
 
+<<<<<<< Updated upstream
+# https://cdec.water.ca.gov/dynamicapp/staMeta?station_id=LIS
 # url prefix and suffix - works for this simple, single endpoint use case
 # hard codes the sensor number (61) and duration (96 hrs)
-prefix <- "http://cdec4gov.water.ca.gov/dynamicapp/selectQuery?Stations=LIS&SensorNums=61&dur_code=E&End="
-suffix <- "&span=168hours"
+# prefix <- "http://cdec4gov.water.ca.gov/dynamicapp/selectQuery?Stations=LIS&SensorNums=61&dur_code=E&End="
+prefix <- "http://cdec4gov.water.ca.gov/dynamicapp/selectQuery?Stations=LIS&SensorNums=1,21,25,28,61,221&dur_code=E&End="
+# suffix <- "&span=48hours"
+suffix <- "&span=8760hours"
+=======
+  # url prefix and suffix
+  prefix <- "https://cdec.water.ca.gov/dynamicapp/QueryF?s=LIS&d="
+suffix <- "+09:00&span=168hours"
+>>>>>>> Stashed changes
 
 # URL query to pass to CDEC
-url <- paste(prefix, today, suffix, sep = "")
+url <- paste(prefix, format(today, "%d-%b-%Y"), suffix, sep = "")
 
 # read the query, unlist the output and clean:
 # rename cols, convert t to datetime, filter blank (future) DO
-df <- read_html(url) %>% 
-  html_elements("tbody") %>% 
-  html_table() %>% 
+page <- read_html(url)
+tables <- page %>% html_nodes("table")
+
+df <- tables[3] %>% 
+  html_table(fill = TRUE) %>% 
   .[[1]] %>% 
-  select(1:2) %>% 
+  <<<<<<< Updated upstream
+select(1, seq(2, ncol(.), 2)) %>% 
+  setNames(
+    c("t",       # time
+      "h_ft",    # 1:   stage (ft)
+      "v_ft_s",  # 21:  velocity (ft/s)
+      "cp_ug_l", # 28:  chlorophyll (ug/L)
+      "t_f",     # 25:  temp (F)
+      "trb_fnu", # 221: turbidity (FNU)
+      "do_mg_l"  # 61:  dissolved oxygen (mg/L)
+    )
+  ) %>% 
+  mutate(
+    across(-t, ~ifelse(.x == "--", NA, .x)),
+    across(-t, ~as.numeric(.x)),
+    t = mdy_hm(t) 
+  )
+
+rownames(df) <- NULL
+=======
+  select(`DATE / TIMEPDT`, `DIS OXY  MG/L`) %>% 
   setNames(c("t", "do")) %>% 
-  mutate(do = ifelse(do == "--", NA, do)) %>% 
-  mutate(t = mdy_hm(t), do = as.numeric(do)) 
+  mutate(
+    t  = mdy_hm(t),
+    do = as.numeric(do)
+  ) %>% 
+  filter(!is.na(t) & !is.na(do))
+>>>>>>> Stashed changes
 cat("Downloaded", nrow(df), "rows of data.\n")
 
 # stash for later
@@ -38,6 +73,7 @@ caption <- paste(
   "\n", "Data obtained from CDEC API (http://cdec4gov.water.ca.gov/)"
 )
 
+# TODO: filter to last 7 days
 # plot and save to GH path, then commit changes
 p <- ggplot() +
   geom_rect(
@@ -61,7 +97,7 @@ p <- ggplot() +
   geom_point(
     data = df, aes(t, do),
     alpha = 0.3, color = "blue"
-    ) + 
+  ) + 
   geom_line(
     data = df, aes(t, do),
     alpha = 0.7
@@ -83,7 +119,7 @@ df_na <- data.frame(
   text = c(NA, "Sensor reported missing data.", NA)
 )
 
-if (nrow(filter(df, !is.na(do))) == 0) {
+if (nrow(df) == 0) {
   p <- df_na %>% 
     ggplot() +
     geom_text(aes(x, y, label = text), size = 10) +
